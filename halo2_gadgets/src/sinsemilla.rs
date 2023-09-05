b5c3adb54b8c2fce24ae707603d2c349dd00501a
Thor Kamphefner <thorck@pm.me>
2023-09-05 14:16:26 -0700

    fmt

--- halo2_gadgets/src/sinsemilla.rs
+++ halo2_gadgets/src/sinsemilla.rs
@@ -655,11 +655,7 @@ pub(crate) mod tests {
                         |(l, (left, right))| {
                             let merkle_crh = sinsemilla::HashDomain::from_Q((*Q).into());
                             let point = merkle_crh
-                                .hash_to_point(
-                                    l.into_iter()
-                                        .chain(left)
-                                        .chain(right),
-                                )
+                                .hash_to_point(l.into_iter().chain(left).chain(right))
                                 .unwrap();
                             point.to_affine()
                         },
